478a877ff858b89ad45fec2ceaf89bae7a22fb1f
leandro <leandro@ebi.ac.uk>
2020-07-30 18:07:28 +0100

    Removing SVTYPE=DNADIFF_SNP; from dnadiff output

--- varifier/dnadiff.py
+++ varifier/dnadiff.py
@@ -61,7 +61,7 @@ def _snps_file_to_vcf(snps_file, query_fasta, outfile):
                         variant.ref_base,
                         ".",
                         ".",
-                        f"SVTYPE=DNADIFF_SNP;QNAME={variant.ref_name};QSTART={variant.ref_start + 1};QSTRAND={'-' if variant.reverse else '+'}",
+                        f"QNAME={variant.ref_name};QSTART={variant.ref_start + 1};QSTRAND={'-' if variant.reverse else '+'}",
                         "GT",
                         "1/1",
                     ]
